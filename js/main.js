863d644a46d7b596ac8d551553952dcf2d464e4e
rammusdev <rammus.shigoto@gmail.com>
2026-07-26 13:32:57 +0200

    v0.5.4 modify main.js for barcode scanner

--- js/main.js
+++ js/main.js
@@ -1,56 +1,59 @@
-import { startCamera, getVideo } from "./camera.js";
-import { cropROI } from "./roi.js";
-import { setStatus } from "./ui.js";
-import { debug, initDebug } from "./debug.js";
-import { startBarcodeScanner } from "./barcode.js";
+import {startCamera,getVideo} from "./camera.js";
+import {cropROI} from "./roi.js";
+import {setStatus} from "./ui.js";
+import {debug,initDebug} from "./debug.js";
+import {startBarcodeScanner} from "./barcode.js";
 
 initDebug();
 
-const startBtn = document.getElementById("startBtn");
-const captureBtn = document.getElementById("captureBtn");
+const startBtn=document.getElementById("startBtn");
+const captureBtn=document.getElementById("captureBtn");
 
-const roiElement = document.getElementById("roi");
-const snapshot = document.getElementById("snapshot");
+const roiElement=document.getElementById("roi");
+const snapshot=document.getElementById("snapshot");
 
-startBtn.onclick = async () => {
-    try {
+startBtn.onclick=async()=>{
+    try{
         await startCamera();
 
         setStatus("相機已開啟");
 
-        const video = getVideo();
+        const video=getVideo();
 
-        debug("Camera", "Started");
+        debug("Camera","Started");
+        debug("Video",`${video.videoWidth} x ${video.videoHeight}`);
 
-        debug(
-            "Video",
-            `${video.videoWidth} x ${video.videoHeight}`
-        );
-
-    } catch (error) {
-        console.error(error);
-
+    }catch(error){
+        debug("Camera Error",error.message);
         setStatus("相機開啟失敗");
-
-        debug(
-            "Camera Error",
-            error.message
-        );
     }
 };
 
-captureBtn.onclick = () => {
+captureBtn.onclick=()=>{
+    const video=getVideo();
 
-    const video = getVideo();
+    debug("Capture","Clicked");
 
     debug(
-        "Capture",
-        "Barcode Mode"
+        "Video",
+        `${video.videoWidth} x ${video.videoHeight}`
     );
+
+    cropROI(
+        video,
+        roiElement,
+        snapshot,
+        debug
+    );
+
+    snapshot.style.display="block";
+
+    debug("ROI","Captured");
 
     startBarcodeScanner(
         video,
         debug
     );
 
+    setStatus("ROI裁切完成");
 };
