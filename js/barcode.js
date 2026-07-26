42d0c03b97b3cd631c02355fb7b37024d6729724
rammusdev <rammus.shigoto@gmail.com>
2026-07-26 13:21:37 +0200

    v0.5 modify scanner situation 2

--- js/barcode.js
+++ js/barcode.js
@@ -1,7 +1,9 @@
 let codeReader=null;
 let scanning=false;
+let timer=null;
 
 export function startBarcodeScanner(video,debug){
+
     if(!window.ZXing){
         debug("Barcode","ZXing not loaded");
         return;
@@ -10,29 +12,63 @@ export function startBarcodeScanner(video,debug){
     if(scanning)return;
 
     scanning=true;
+
     codeReader=new ZXing.BrowserMultiFormatReader();
+
+    const canvas=document.createElement("canvas");
+    const ctx=canvas.getContext("2d");
 
     debug("Barcode","Scanner Ready");
 
-    codeReader.decodeFromVideoElement(video)
-    .then(result=>{
-        debug("Barcode",result.getText());
-        stopBarcodeScanner();
-    })
-    .catch(error=>{
-        if(scanning){
-            requestAnimationFrame(()=>{
-                startBarcodeScanner(video,debug);
-            });
+    function scan(){
+
+        if(!scanning)return;
+
+        if(video.videoWidth===0){
+            timer=requestAnimationFrame(scan);
+            return;
         }
-    });
+
+        canvas.width=video.videoWidth;
+        canvas.height=video.videoHeight;
+
+        ctx.drawImage(
+            video,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+
+        codeReader.decodeFromCanvas(canvas)
+        .then(result=>{
+
+            debug(
+                "Barcode",
+                result.getText()
+            );
+
+            stopBarcodeScanner();
+
+        })
+        .catch(()=>{
+
+            timer=requestAnimationFrame(scan);
+
+        });
+    }
+
+    scan();
 }
 
 export function stopBarcodeScanner(){
+
     scanning=false;
 
-    if(codeReader){
-        codeReader.reset();
-        codeReader=null;
+    if(timer){
+        cancelAnimationFrame(timer);
+        timer=null;
     }
+
+    codeReader=null;
 }
